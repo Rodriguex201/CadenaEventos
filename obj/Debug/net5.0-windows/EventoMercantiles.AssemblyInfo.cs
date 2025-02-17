6abf7f315fe9da6b85a637895228e22e9b3cdfda
Emanuel García Rodríguez <emanuelgr150@gmail.com>
2025-02-17 08:53:22 -0500

    este

--- obj/Debug/net5.0-windows/EventoMercantiles.AssemblyInfo.cs
+++ obj/Debug/net5.0-windows/EventoMercantiles.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventoMercantiles")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0e15e7397e3bdac7210722c94d9fe2fb1a764f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventoMercantiles")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventoMercantiles")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
